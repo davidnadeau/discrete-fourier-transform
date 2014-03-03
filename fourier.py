3d29dfcd3b95c02e17ce16b3fb842ff0c4443afb
soote <waffleee@gmail.com>
2014-03-03 07:48:49 -0500

    working c implementation

--- fourier.py
+++ fourier.py
@@ -12,64 +12,64 @@ import time
 
 
 def readfile(file):
-    #open file
-    txt = open(file, "r")
-    #read header information
-    header = readheader(txt)
-    #read samples into array
-    samples = readsamples(txt, header['samples'])
-    #free file resource
-    txt.close()
-    #return samples
-    return {
-        "header": header,
-        "samples": samples
-    }
+	#open file
+	txt = open(file, "r")
+	#read header information
+	header = readheader(txt)
+	#read samples into array
+	samples = readsamples(txt, header['samples'])
+	#free file resource
+	txt.close()
+	#return samples
+	return {
+		"header": header,
+		"samples": samples
+	}
 
 def readheader(txt):
-    return {
-        "samples":      int (readcolumn(txt, 1)),
-        "bps":          int (readcolumn(txt, 1)),
-        "channels":     int (readcolumn(txt, 1)),
-        "samplerate":   int (readcolumn(txt, 1)),
-        "normalized":   readcolumn(txt, 1)
-    }    
+	return {
+		"samples":      int (int (readcolumn(txt, 1))/10),
+		"bps":          int (readcolumn(txt, 1)),
+		"channels":     int (readcolumn(txt, 1)),
+		"samplerate":   int (readcolumn(txt, 1)),
+		"normalized":   readcolumn(txt, 1)
+	}    
 
 #read a single column from a text file
 def readcolumn(txt, i):
-    return txt.readline().split()[i]
+	return txt.readline().split()[i]
 
 #read every sample, return 2d list [ [c1, c2], [c1,c2], ... ]
 #where: 
 #   [i][0] is the first channel of sample i
 #   [i][1] is the second channel of sample i
 def readsamples(txt, samples):
-    return [readsample(txt) for x in range(samples)]
+	return [readsample(txt) for x in range(samples)]
 
 #read a line from the txt file, return array of words (split on whitespace)
 def readsample(txt):
-    return txt.readline().split()
+	return txt.readline().split()
 
 #####################################
 #   WRITE TO TEXT                   #
 #####################################
 def writewave(filename, w, wave):
-    file = open(filename, 'w+')
-    file.write(createheader(w))
-    for i in range(round(w['header']['samples'])):
-        file.write(str(int(wave[i][0])))
-        if w['header']['channels'] == 2:
-           file.write('\t' + str(int(wave[i][1])))
-        file.write('\n')
-    file.close()
+	file = open(filename, 'w+')
+	file.write(createheader(w))
+	for i in range(int(round(w['header']['samples']))):
+		file.write(str(int(wave[i][0])))
+		if w['header']['channels'] == 2:
+		   file.write('\t' + str(int(wave[i][1])))
+		file.write('\n')
+	file.close()
 
 def createheader(w):
-    header =  "SAMPLES:\t" + str(w['header']['samples']) + "\n"
-    header += "BITSPERSAMPLE:\t"+ str(w['header']['bps']) + "\n"
-    header += "CHANNELS:\t"+ str(w['header']['channels']) + "\n" 
-    header += "SAMPLERATE:\t"+ str(w['header']['samplerate']) + "\n"
-    header += "NORMALIZED:\t"+ str(w['header']['normalized']) + "\n"
-    return header 
+	header =  "SAMPLES:\t" + str(w['header']['samples']) + "\n"
+	header += "BITSPERSAMPLE:\t"+ str(w['header']['bps']) + "\n"
+	header += "CHANNELS:\t"+ str(w['header']['channels']) + "\n" 
+	header += "SAMPLERATE:\t"+ str(w['header']['samplerate']) + "\n"
+	header += "NORMALIZED:\t"+ str(w['header']['normalized']) + "\n"
+	return header 
 
 #####################################
 #   SUMMING WAVES                   #
@@ -77,17 +77,17 @@ def createheader(w):
 
 #add n waves
 def foldwaves(waves, channels):
-    origwave = []
-    print len (waves)
-    for w in waves:
-        ch = []
-        for c in range(channels):
-            value = 0
-            for s in w:
-                value += s[c]['amplitude']
-            ch.append(value)
-        origwave.append(ch)
-    return origwave 
+	origwave = []
+	print len (waves)
+	for w in waves:
+		ch = []
+		for c in range(channels):
+			value = 0
+			for s in w:
+				value += s[c]['amplitude'] 
+			ch.append(value)
+		origwave.append(ch)
+	return origwave 
 
 #####################################
 #   DISCRETE FOURIER TRANSFORM      #
@@ -100,49 +100,38 @@ def foldwaves(waves, channels):
 #####################################
 
 def reconstructdft(w, T, channels):
-    return foldwaves(w, channels)
+	return foldwaves(w, channels)
 
-def deconstructdft(w, T, quality,channels):
-    waves = []
-    for t in range(T):
-        harmonics = []
-        for n in range(1, quality):
-            c = []
-            an = 0
-            bn = 0
-            for i in range(channels):
-                for j in range(T):
-                    #written in c, included in header
-                    an += fouriertransform.an(int(w[j][i]), n, j)
-                    bn += fouriertransform.bn(int(w[j][i]), n, j)
-                a0 = geta0(T, w, i) 
-                an = an * (2/T) 
-                bn = bn * (2/T) 
-                
-                p1 = an * math.cos((2*math.pi*n*t) / T)
-                p2 = bn * math.sin((2*math.pi*n*t) / T)
-                c.append({
-                    "number": n,
-                    "coefficients": {"a": an, "b": bn},
-                    "amplitude": p1 + p2 + a0
-                })
-            harmonics.append(c)
-        waves.append(harmonics) 
-        print len(waves)
-    return waves
-
-def geta0(T, x, channel):
-    sum = 0;
-    for i in range(T):
-        sum += int(x[i][channel])
-    return sum * (1/T)
+def deconstructdft(w, T, quality, channels):
+	waves = []
+        #written in c to calc a0
+	a0 = [fouriertransform.a0(w,T,0), fouriertransform.a0(w,T,1)]
+        print a0
+	for t in range(T):
+		harmonics = []
+		for n in range(1, quality):
+			c = []
+			for i in range(channels):
+				#written in c to calc an and bn
+				coefficients = fouriertransform.coefficients(w, n, T, i)
+				p1 = coefficients[0] * math.cos((2*math.pi*n*t) / T)
+				p2 = coefficients[1] * math.sin((2*math.pi*n*t) / T)
+				c.append({
+					"number": n,
+					"coefficients": coefficients,
+					"amplitude": p1 + p2 + a0[i]
+				})
+			harmonics.append(c)
+		waves.append(harmonics)
+                print len(waves)
+	return waves
 
 if __name__ == "__main__":
-    w1 = readfile("sine-440.txt")
-    #print("Original wave:\n", w1['samples'], '\n')
-    dftwaves = deconstructdft(w1['samples'], w1['header']['samples'], 2, w1['header']['channels'])
+	w1 = readfile("sine-440-short.txt")
+	print("Original wave:\n", w1['samples'], '\n')
+	dftwaves = deconstructdft(w1['samples'], w1['header']['samples'], int(w1['header']['samples']/10), w1['header']['channels'])
 
-    #print "DFT waves:\n", dftwaves, '\n'
-    wave = reconstructdft(dftwaves, w1['header']['samples'], w1['header']['channels'])
-    #print("Reconstructed wave:\n", wave, '\n')
-    writewave("tetwtwet", w1, wave)
+	#print "DFT waves:\n", dftwaves, '\n'
+	wave = reconstructdft(dftwaves, w1['header']['samples'], w1['header']['channels'])
+	#print("Reconstructed wave:\n", wave, '\n')
+	writewave("tetwtwet", w1, wave)
--- fouriertransform.c
+++ fouriertransform.c
@@ -3,34 +3,62 @@
 
 #define PI 3.14159265
 
-static PyObject *fourier_an(PyObject *self, PyObject *args) {
+static PyObject *fourier_getcoefficients(PyObject *self, PyObject *args) {
     int n;
-    int x;
+    PyObject *x;
+    PyObject *samplechannels;
+    PyObject *sample;
+    int T;
+    int channel;
     int i;
-
-    if (!PyArg_ParseTuple(args, "iii", &x,  &n, &i)) {
+    double an = 0;
+    double bn = 0;
+    char *samp;
+    double samplevalue;
+    
+    if (!PyArg_ParseTuple(args, "Oiii", &x,  &n, &T, &channel)) {
         return NULL;
     }
     
-    return Py_BuildValue("i", x * cos( (2*PI*n*i) ));
+    for (i = 1; i < T; ++i) { 
+        samplechannels = PySequence_GetItem(x,i);
+        sample = PySequence_GetItem(samplechannels, channel);
+        PyArg_Parse(sample, "s", &samp);
+        samplevalue = atoi(samp);
+        an += samplevalue * cos( (2.0*PI*n*i) );
+        bn += samplevalue * sin( (2.0*PI*n*i) );
+    }
+    return Py_BuildValue("dd", an * (2.0/T), bn *(2.0/T));
 }
 
-static PyObject *fourier_bn(PyObject *self, PyObject *args) {
+static PyObject *fourier_a0(PyObject *self, PyObject *args) {
     int n;
-    int x;
+    PyObject *x;
+    PyObject *samplechannels;
+    PyObject *sample;
+    int T;
+    int channel;
     int i;
+    double a0 = 0;
+    char *samp;
 
-    if (!PyArg_ParseTuple(args, "iii", &x,  &n, &i)) {
+    if (!PyArg_ParseTuple(args, "Oii", &x, &T, &channel)) {
         return NULL;
     }
-    
-    return Py_BuildValue("i", x * sin( (2*PI*n*i) ));
+    for (i = 0; i < T; ++i) { 
+        samplechannels = PySequence_GetItem(x,i);
+        sample = PySequence_GetItem(samplechannels, channel);
+        PyArg_Parse(sample, "s", &samp);
+        a0 += atoi (samp);
+    }
+
+    return Py_BuildValue("d", a0 / T);
 }
 
 static PyMethodDef fourier_funcs[] = {
-    {"an", (PyCFunction)fourier_an, 
+    {"coefficients", (PyCFunction)fourier_getcoefficients, 
         METH_VARARGS, NULL},
-    {"bn", (PyCFunction)fourier_bn, 
+    {"a0", (PyCFunction)fourier_a0, 
         METH_VARARGS, NULL},
     {NULL}
 };
